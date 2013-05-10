4c49dcfd68ff0e349adf69f6ee0fef66ed43812f
Federico Franco <fede_franco_93@hotmail.com>
2013-05-10 11:04:12 -0300

    version 2.0

--- inmo-master/config/schema.sql
+++ inmo-master/config/schema.sql
@@ -1,30 +1,44 @@
-DROP TABLE IF EXISTS Owners;
-CREATE TABLE Owners(
-    idOwner  int(11) NOT NULL  auto_increment PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    direccion varchar(60),
-    email varchar(80),
-    inmobiliarias varchar(200)
+DROP TABLE IF EXISTS owners;
+CREATE TABLE owners(
+	id  smallint(100) not null auto_increment PRIMARY KEY,
+	first_name VARCHAR(50) not null,
+	last_name VARCHAR(50) not null,
+	dni int(15),
+	email varchar(40),
+	street varchar(45) not null,
+	neighborhood varchar(45) not null,
+	city varchar(45) not null
 );
 
-drop table if exists realStates;
-create table realStates(
-	idRS int(11) not null auto_increment primary key,
-	nombre varchar(40),
-	direccion varchar(60),
-	telefono integer(12),
-    email varchar(70),
-    website varchar(60),
-    owners varchar(200)
+drop table if exists real_estates;
+create table real_estates(
+	id smallint(100) not null auto_increment primary key,
+	name varchar(40) not null,
+	telephone integer(12) not null,
+	email varchar(40),
+	website varchar(60) not null,
+	street varchar(45) not null,
+	neighborhood varchar(45) not null,
+	city varchar(45) not null
 );
 
-drop table if exists Buildings;
-create table Buildings(
-	idBuilding int(11) not null auto_increment primary key,
-	tipo enum('Departamento','Casa','Local','Cochera'),
-	direccion varchar(60),
-    descripcion varchar(400),
-    precio float,
-	estado enum('Venta','Alquiler')
+drop table if exists buildings;
+create table buildings(
+	id smallint(100) not null auto_increment primary key,
+	type enum('Land','Farm','House','Departament','Garage') not null,	
+	owner_id smallint(100) not null references owners(id),
+	description varchar(400),
+	price float not null,
+	status enum('Sell','Rent') not null,
+	street varchar(45) not null,
+	neighborhood varchar(45) not null,
+	city varchar(45) not null
+);
+
+drop table if exists owners_has_real_states;
+create table owners_has_real_states(
+	owner_dni int(15) references owners(dni),
+	owner_id smallint(100) references owners(idOwner),
+	real_states_id smallint(100) references real_states(idRE),
+	RS_name varchar(40) references real_states(name)
 );
---
+++ inmo-master/src/test/java/com/unrc/app/OwnersSpec.java
@@ -0,0 +1,42 @@
+/*package com.unrc.app;
+
+import com.unrc.app.models.Owners;
+
+import org.javalite.activejdbc.Base;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.javalite.test.jspec.JSpec.the;
+
+public class OwnersSpec{
+
+    @Before
+    public void before(){
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+        Base.openTransaction();
+    }
+
+    @After
+    public void after(){
+        Base.rollbackTransaction();
+        Base.close();
+    }
+
+    @Test
+    public void shouldValidateMandatoryFields(){
+
+        Owners owner = new Owners();
+
+        //check errors
+        the(owner).shouldNotBe("valid");
+        the(owner.errors().get("first_name")).shouldBeEqual("value is missing");
+        the(owner.errors().get("last_name")).shouldBeEqual("value is missing");
+
+        //set missing values
+        owner.set("first_name", "Fede", "last_name", "Franco");
+
+        //all is good:
+        the(owner).shouldBe("valid");
+    }
+}*/
